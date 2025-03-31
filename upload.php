bf04c4a53557473c93e29d8ac907bac30b604476
unknown <nazarld@yandex.ru>
2025-03-31 08:44:13 +0300

    rework

--- upload.php
+++ upload.php
@@ -1,7 +1,17 @@
 <?php
 declare(strict_types= 1);
 
-$fileName = $_POST['file_name'];
+if (empty($_POST['file_name'])) {
+    header('Location: ./index.php');
+} else {
+    $fileName = $_POST['file_name'];
+}
+
+if (!isset($_FILES['content']) || $_FILES['content']['error'] !== UPLOAD_ERR_OK) {
+    header('Location: ./index.php');
+    exit;
+}
+
 $ext = pathinfo($_FILES['content']['name'], PATHINFO_EXTENSION);
 $fileSize = round($_FILES['content']['size'] / 1000, 1);
 $uploadDir = './uploads/';
@@ -11,7 +21,11 @@ $pathToShow = trim($uploadDir, '.') . $fileName . '.' . $ext;
 if (!$_FILES['content']['tmp_name'] || !$fileName) {
     header('Location: ./index.php');
 } else {
-    move_uploaded_file($_FILES['content']['tmp_name'], $uploadFile);
-    echo 'Файл сохранен в: '. $_SERVER['HTTP_ORIGIN'] . $pathToShow . '<br>';
-    echo 'Размер файла: ' . $fileSize . ' КБ';
+
+    if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadFile)) {
+        echo 'Файл сохранен в: '. $_SERVER['HTTP_ORIGIN'] . $pathToShow . '<br>';
+        echo 'Размер файла: ' . $fileSize . ' КБ';
+    } else {
+        header('Location: ./index.php');
+    }
 }
